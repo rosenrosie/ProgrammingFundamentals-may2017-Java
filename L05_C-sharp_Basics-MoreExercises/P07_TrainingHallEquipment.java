bf25fdf49551ec82d2b3f79065692ce7acf61b6d
Todor Chenkov <joto_to@mail.bg>
2017-06-01 00:27:08 +0300

    -add P07_TrainingHallEquipment

---
+++ L05_C-sharp_Basics-MoreExercises/P07_TrainingHallEquipment.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+/**
+ * Created by todor on 1.06.2017 г..
+ */
+public class P07_TrainingHallEquipment {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double budget = Double.parseDouble(scan.nextLine());
+        int itemsCount = Integer.parseInt(scan.nextLine());
+        double subtotal = 0;
+        for (int i = 0; i < itemsCount; i++) {
+            String productName = scan.nextLine();
+            double productPrice = Double.parseDouble(scan.nextLine());
+            int productCount = Integer.parseInt(scan.nextLine());
+            
+            productName += productCount > 1 ? "s" : "";
+            
+            System.out.printf("Adding %d %s to cart.%n", productCount, productName);
+            subtotal += (productPrice * productCount);
+        }
+        
+        System.out.printf("Subtotal: $%.2f%n", subtotal);
+        
+        System.out.println(
+                budget >= subtotal ?
+                        String.format("Money left: $%.2f%n", budget - subtotal) :
+                        String.format("Not enough. We need $%.2f more.%n", subtotal - budget)
+        );
+    }
+}
